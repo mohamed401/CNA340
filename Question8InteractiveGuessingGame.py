9c85e724e417ab67bf5db200fb302d5923f77324
1kalisem1 <50159960+1kalisem1@users.noreply.github.com>
2019-05-16 12:12:42 -0700

    Add files via upload

---
+++ Question8InteractiveGuessingGame.py
@@ -0,0 +1,17 @@
+import random
+
+randomnumber = random.randint(1,10)
+print(randomnumber)
+
+for i in range (1,4)
+    userguess = input("What number do you guess?")
+    userguess = int(userguess)
+    if randomnumber == userguess:
+        print("Congradulation! Your guess is correct!")
+        break
+    elif randomnumber < userguess:
+        print("Sorry, your guess is too high!")
+    else:
+        print("Sorry, your guess is too low!")
+
+
